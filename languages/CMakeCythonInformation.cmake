be78e080b95e529acedccc17dc00937d9a63863a
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2018-10-14 16:26:27 -0700

    :construction: More work on Cython support.
    A few features are still needed if we're to generate these files
    correctly. While we can dump the output in, guaranteeing it works in all
    cases will be difficult. This is simply due to the way in which we build
    via cython. What may have to happen is I end up writing a
    cython-cc/cython-cxx wrapper that knows how to call out to the various
    compilers and can also generate a .d file all in one go. It'd probably
    help in the long run, to be quite honest. But that's for another weekend
    project.

--- languages/CMakeCythonInformation.cmake
+++ languages/CMakeCythonInformation.cmake
@@ -1,30 +1,29 @@
-include(CheckLanguauge)
-
-check_language(CXX)
-check_language(C)
-
-if (NOT C AND NOT CXX)
-  message(FATAL_ERROR "Cython cannot be enabled without C or C++ support")
-endif()
-
-if (CMAKE_C_COMPILER)
-  set(CMAKE_Cython_CXX)
-  set(CMAKE_Cython_COMPILE_SOURCE ${CMAKE_C_COMPILE_OBJECT})
-endif()
-
-if (CMAKE_CXX_COMPILER)
-  set(CMAKE_Cython_CXX "--cplus")
-  set(CMAKE_Cython_COMPILE_SOURCE ${CMAKE_CXX_COMPILE_OBJECT})
-endif()
-
-
 # CMake doesn't let us automatically set up the compiler dependencies, but
 # luckily those files will stick around and if they change they need to be
 # recompiled anyhow. So we just *append* another execution rule to ours.
 # (Special thanks to Rob Maynard for pointing this out)
 # Also, we're only ever going to support Python 3 or later :P
 # OK, maybe not :v
-set(CMAKE_Cython_COMPILE_OBJECT
-  "<CMAKE_Cython_COMPILER> -3 ${CMAKE_Cython_CXX} <INCLUDES> <FLAGS> -o <OBJECT>")
+include(Dump)
 
-list(APPEND CMAKE_Cython_COMPILER_OBJECT ${CMAKE_Cython_COMPILE_SOURCE})
+set(CMAKE_Cython_COMPILE_OBJECT
+  "<CMAKE_Cython_COMPILER> -${CMAKE_Cython_SYNTAX} ${CMAKE_Cython_CXX} <INCLUDES> <FLAGS> -o <OBJECT> <SOURCE>")
+
+message(STATUS "LANG: ${CMAKE_Cython_LANG}")
+list(APPEND CMAKE_Cython_COMPILE_OBJECT ${CMAKE_${CMAKE_Cython_LANG}_COMPILE_OBJECT})
+set(CMAKE_Cython_CREATE_STATIC_LIBRARY ${CMAKE_${CMAKE_Cython_LANG}_STATIC_LIBRARY})
+set(CMAKE_Cython_CREATE_SHARED_MODULE ${CMAKE_${CMAKE_Cython_LANG}_CREATE_SHARED_MODULE})
+
+dump(CMAKE_Cython_CREATE_STATIC_LIBRARY)
+dump(CMAKE_Cython_CREATE_SHARED_MODULE)
+dump(CMAKE_Cython_COMPILE_OBJECT)
+
+if (NOT DEFINED CMAKE_Cython_ARCHIVE_CREATE)
+  set(CMAKE_Cython_ARCHIVE_CREATE "<CMAKE_AR> qc <TARGET> <LINK_FLAGS> <OBJECTS>")
+endif()
+if (NOT DEFINED CMAKE_Cython_ARCHIVE_APPEND)
+  set(CMAKE_Cython_ARCHIVE_APPEND "<CMAKE_AR> q <TARGET> <LINK_FLAGS> <OBJECTS>")
+endif()
+if (NOT DEFINED CMAKE_Cython_ARCHIVE_FINISH)
+  set(CMAKE_Cython_ARCHIVE_FINISH "<CMAKE_RANLIB> <TARGET>")
+endif()
--- languages/CMakeTestCythonCompiler.cmake
+++ languages/CMakeTestCythonCompiler.cmake
@@ -11,6 +11,7 @@ endif()
 
 set(test_root "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}")
 set(test_file "${test_root}/CMakeTmp/testCythonCompiler.pyx")
+set(test_obj "${test_file}.${CMAKE_Cython_HOST_EXTENSION}")
 
 PrintTestCompilerStatus("Cython" "${CMAKE_Cython_COMPILER}")
 file(WRITE ${test_file} [=[
@@ -32,14 +33,42 @@ cpdef enum CheeseState:
   runny = 3
 ]=])
 
-try_compile(CMAKE_Cython_COMPILER_WORKS ${CMAKE_BINARY_DIR} "${test_file}"
-  OUTPUT_VARIABLE __Cython_COMPILER_OUTPUT)
+include(Dump)
+
+# Because we can't compile correctly unless we also have a C++ compiler,
+# We *run* the tool first, but then use the output to do a try_compile.
+execute_process(COMMAND
+  ${CMAKE_Cython_COMPILER}
+  -${CMAKE_Cython_SYNTAX}
+  ${CMAKE_Cython_CXX}
+  --embed # So we don't have to rock the boat
+  -o "${test_obj}"
+  "${test_file}"
+  RESULT_VARIABLE CMAKE_Cython_COMPILER_WORKS
+  OUTPUT_VARIABLE __Cython_COMPILER_OUTPUT
+  ERROR_VARIABLE __Cython_COMPILER_OUTPUT)
+
+dump(CMAKE_Cython_COMPILER_WORKS)
+
+if (NOT CMAKE_Cython_COMPILER_WORKS)
+  set(_original ${CMAKE_TRY_COMPILE_TARGET_TYPE})
+  set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+  if (MINGW AND CMAKE_Cython_CXX)
+    set(__hack COMPILE_DEFINITIONS -D_hypot=hypot)
+  endif()
+  try_compile(CMAKE_Cython_COMPILER_WORKS ${CMAKE_BINARY_DIR} "${test_obj}"
+    CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:STRING=${Python_INCLUDE_DIRS}"
+    ${__hack}
+    OUTPUT_VARIABLE __Cython_COMPILER_OUTPUT)
+  set(CMAKE_TRY_COMPILE_TARGET_TYPE ${_original})
+endif()
 
 set(CMAKE_Cython_COMPILER_WORKS ${CMAKE_Cython_COMPILER_WORKS})
 unset(CMAKE_Cython_COMPILER_WORKS CACHE)
 set(Cython_TEST_WAS_RUN 1)
 
 if (NOT CMAKE_Cython_COMPILER_WORKS)
+  PrintTestCompilerStatus("Cython" " -- broken")
   file(APPEND ${test_root}/CMakeError.log
     "Determining if the Cython compiler works failed with "
     "the following output:\n ${__Cython_COMPILER_OUTPUT}")
@@ -60,10 +89,9 @@ if (Cython_TEST_WAS_RUN)
 endif()
 
 configure_file(
-  ${CMAKE_CURRENT_LIST_DIR}/CMakeCython.cmake.in
+  ${CMAKE_CURRENT_LIST_DIR}/CMakeCythonCompiler.cmake.in
   ${CMAKE_PLATFORM_INFO_DIR}/CMakeCythonCompiler.cmake
   @ONLY)
 include(${CMAKE_PLATFORM_INFO_DIR}/CMakeCythonCompiler.cmake)
 
 unset(__Cython_COMPILER_OUTPUT)
-
