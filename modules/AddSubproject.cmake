fdb1d8aad830f1f73de20eea773350e14577a2c4
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2018-10-15 12:44:30 -0700

    :bug: Fix typos in AddSubproject
    :bug: Fix typos in AddGitSubproject modules

--- modules/AddSubproject.cmake
+++ modules/AddSubproject.cmake
@@ -4,5 +4,5 @@ include(Fetch)
 
 function (add_subproject name)
   fetch(${name} ${ARGN})
-  add_subdirectory(${name}_SOURCE_DIR ${name}_BINARY_DIR EXCLUDE_FROM_ALL)
+  add_subdirectory(${${name}_SOURCE_DIR} ${${name}_BINARY_DIR} EXCLUDE_FROM_ALL)
 endfunction ()
