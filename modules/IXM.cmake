1b77751860297ee812be14449ef6fa77e27567ed
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2018-11-04 00:55:10 -0700

    :sparkles: Add IXM module (better name than "Utility")

---
+++ modules/IXM.cmake
@@ -0,0 +1,23 @@
+include_guard(GLOBAL)
+
+#[[ SYNOPSIS
+
+This module holds functions and macros related to augmenting CMake. Most, if
+not all of the other modules include this one.
+
+]]
+
+include(PushState)
+
+push_module_state(IXM)
+include(SourceDepends)
+include(ParentScope)
+include(Standalone)
+include(AddPackage)
+include(Algorithm)
+include(ArgParse)
+include(Override)
+include(Print)
+include(Dump)
+include(Fetch)
+pop_module_state()
