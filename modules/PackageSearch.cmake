a0a25d9ea052745cd1edec14d354d11b6a5c06ec
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2018-11-03 17:05:33 -0700

    :sparkles: Add PackageSearch module
    :truck: Renamed FindPackage to PackageSearch
    :truck: Moved *all* relevant modules to PackageSearch.

--- modules/PackageSearch.cmake
+++ modules/PackageSearch.cmake
@@ -1,11 +1,22 @@
 include_guard(GLOBAL)
 
+include(PushState)
+
 #[[ SYNOPSIS
 
 This module is an amalgamation of various functions and macros to aid in
 implementing find_package for libraries, executables, and frameworks (macOS)
 
 ]]
+
+push_module_path(PackageSearch)
+include(Component) # Formerly CheckComponent
+include(Library) # Formerly ImportLibrary
+include(Program) # Formerly ImportExecutable
+include(Check) # Formerly CheckFindPackage
+include(Hide)
+include(Halt)
+pop_module_path()
 
 include(PushFrameworkState)
 include(PushFindState)
--- modules/PushState/FindFramework.cmake
+++ modules/PushState/FindFramework.cmake
@@ -6,12 +6,12 @@ Helper macro to help implement find_package calls for frameworks,
 effectively doing so in one line.
 
 ]]
-macro (push_framework_state value)
+macro (push_find_framework value)
   list(APPEND IXM_CMAKE_FIND_FRAMEWORK ${value})
   set(CMAKE_FIND_FRAMEWORK ${value})
 endmacro()
 
-macro (pop_framework_state value)
+macro (pop_find_framework value)
   list(GET IXM_CMAKE_FIND_FRAMEWORK -1 CMAKE_FIND_FRAMEWORK)
   list(REMOVE_AT IXM_CMAKE_FIND_FRAMEWORK -1)
 endmacro()
