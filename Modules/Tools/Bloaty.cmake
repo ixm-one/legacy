6303b5a321e59c6169c344867d7d9e2a499574f6
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2019-01-13 07:13:20 -0800

    :fire: Remove unnecessary files

--- Modules/Tools/Bloaty.cmake
+++
@@ -1,6 +0,0 @@
-include_guard(GLOBAL)
-
-include(IXM)
-find_package(Bloaty)
-
-halt_unless(Bloaty FOUND)
--- Modules/Tools/DistCC.cmake
+++
@@ -1,9 +0,0 @@
-include_guard(GLOBAL)
-
-include(IXM)
-
-find_package(DistCC)
-
-halt_unless(DistCC FOUND)
-
-set_property(GLOBAL APPEND PROPERTY IXM_COMPILER_LAUNCHERS distcc)
