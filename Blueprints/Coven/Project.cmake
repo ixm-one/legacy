fbcfb1011af39a3449f2a9c0db0dab72f5df72a5
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2020-01-15 04:46:05 -0800

    ♻ Refactor Blueprints/Coven/Project to be more correct
    There's still a few things left to do.
    
    For starters, we need to rename this file to Targets.cmake. This only
    creates the targets, sets up the initial settings, and then finishes.
    This isn't project wide for the initialization, and some additional
    things like installing, setting components, etc. are needed.
    
    Additionally, the ability to detect *and* build directories that hold a
    main.<ext> file is still not implemented. This *is* needed for
    generating additional binaries that are to be installed or exported for
    the program.
    
    Lastly, a solution is needed for how tools (subprojects/binaries that
    are ALWAYS built by the project in a "native" way, even when cross
    compiling) are going to interact with the Coven layout. We don't
    currently have a single path for "here's my 'build scripts', run this
    separate project via try_compile, then import all its exported objects
    as if they had been installed". It's frustrating. You'd think the CMake
    devs would have a better solution than "lmao, just use an emulator
    dummy", but then again this is the same set of developers that think
    filesystem entries can be represented as a list of doubles in JSON, and
    don't understand how to actually export JSON in a usable format. Nor do
    they know how to write a fucking programming language but that ship has
    sailed.
    
    I shouldn't be a leading expert in CMake. I'm self taught, mentally ill, and
    without a degree and somehow I still run circles around most of their devs.
    What the fuck is their excuse? It's so disheartening. 😟

--- Blueprints/Coven/Project.cmake
+++ Blueprints/Coven/Project.cmake
@@ -1,5 +1,8 @@
 include_guard(GLOBAL)
 
+# TODO: Rename this file to Targets.cmake, as it better fits what this file
+# actually does :)
+
 function (coven_project_init)
   coven_project_interface() # Generates 'PROJECT_NAME::interface'
   coven_project_library() # Generates 'PROJECT_NAME::library'
@@ -14,83 +17,47 @@ function (coven_project_interface)
   add_library(${PROJECT_NAME} INTERFACE)
 
   add_library(${PROJECT_NAME}::interface ALIAS ${PROJECT_NAME})
-
   set_property(TARGET ${PROJECT_NAME} PROPERTY EXPORT_NAME interface)
 
   target_include_directories(${PROJECT_NAME}
     INTERFACE
       $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
       $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
-  target_include_directories(${PROJECT_NAME}
-    INTERFACE
-      $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>)
-  set_property(GLOBAL APPEND PROPERTY
-    coven::${PROJECT_NAME}::install ${PROJECT_NAME})
-endfunction()
-
-function (coven_project_name)
-  set(primary ${PROJECT_NAME}-library)
-  if (NOT TARGET ${PROJECT_NAME}-library)
-    set(primary ${PROJECT_NAME})
-  endif()
-  add_library(${PROJECT_NAME}::${PROJECT_NAME} ALIAS ${primary})
 endfunction()
 
 function (coven_project_library)
-  file(GLOB files
-    LIST_DIRECTORIES OFF
-    CONFIGURE_DEPENDS
-    "${PROJECT_SOURCE_DIR}/src/*")
+  file(GLOB_RECURSE files CONFIGURE_DEPENDS "${PROJECT_SOURCE_DIR}/src/*")
   list(FILTER files EXCLUDE REGEX "^${PROJECT_SOURCE_DIR}/src/main[.].*$")
   list(FILTER files EXCLUDE REGEX "^${PROJECT_SOURCE_DIR}/src/bin/.*$")
   if (NOT files)
     return()
   endif()
-  # This needs to be a separate command...
-  coven_library_create(name)
-  target(SOURCES ${name} PRIVATE ${files})
+  set(name ${PROJECT_NAME}-library)
+  add_library(${name})
+  add_library(${PROJECT_NAME}::library ALIAS ${name})
+  target_link_libraries(${name}
+    PUBLIC
+      ${PROJECT_NAME}::interface
+    PRIVATE
+      ${PROJECT_NAME}::private)
+  set_target_properties(${name}
+    PROPERTIES
+      OUTPUT_NAME ${PROJECT_NAME}
+      EXPORT_NAME ${PROJECT_NAME})
 endfunction()
 
 #[[Generates object libraries]]
 function (coven_project_objects)
-  file(GLOB entries LIST_DIRECTORIES ON CONFIGURE_DEPENDS "${PROJECT_SOURCE_DIR}/src/*")
+  file(GLOB entries CONFIGURE_DEPENDS "${PROJECT_SOURCE_DIR}/src/*")
   foreach (entry IN LISTS entries)
     if (IS_DIRECTORY "${entry}")
       list(APPEND directories "${entry}")
     endif()
   endforeach()
   list(REMOVE_ITEM directories "${PROJECT_SOURCE_DIR}/src/bin")
+  list(APPEND directories "${PROJECT_SOURCE_DIR}/src")
   foreach (directory IN LISTS directories)
-    file(GLOB sources CONFIGURE_DEPENDS "${directory}/*")
-    # FIXME: Should we support main.<ext> as a "thing" for additional executables?
-    list(FILTER sources EXCLUDE REGEX "^${directory}/main[.].*$")
-    if (NOT sources)
-      continue()
-    endif()
-    get_filename_component(name "${directory}" NAME)
-    set(name ${PROJECT_NAME}-library-${name})
-    add_library(${name} OBJECT)
-    set_property(TARGET ${name} PROPERTY EXCLUDE_FROM_ALL ON)
-    target(SOURCES ${name} PRIVATE ${sources})
-    target_include_directories(${name}
-      PUBLIC
-        $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
-      PRIVATE
-        $<BUILD_INTERFACE:${directory})
-    string(CONCAT when $<IF:
-      $<BOOL:$<TARGET_PROPERTY:${name},WHEN>>,
-      $<TARGET_GENEX_EVAL:
-        ${name},
-        $<TARGET_PROPERTY:${name},WHEN>
-      >,
-      $<BOOL:ON>
-    >)
-    if (NOT TARGET ${PROJECT_NAME}::library)
-      coven_library_create(library)
-    endif()
-    target_sources(${library}
-      PRIVATE
-        $<${when}:$<TARGET_OBJECTS:${name}>>)
+    coven_project_module(${directory})
   endforeach()
 endfunction()
 
@@ -112,26 +79,70 @@ function (coven_project_program)
     PROPERTIES
       OUTPUT_NAME ${PROJECT_NAME}
       EXPORT_NAME ${PROJECT_NAME})
-  set_property(GLOBAL APPEND PROPERTY coven::${PROJECT_NAME}::install ${name})
 endfunction()
 
 #[[Generates any additional 'runtime' targets located in src/bin]]
 function (coven_project_runtime)
-  file(GLOB files CONFIGURE_DEPENDS "${PROJECT_SOURCE_DIR}/src/bin/*")
-  foreach (file IN LISTS files)
-    get_filename_component(base "${file}" NAME_WE)
-    set(name ${PROJECT_NAME}-program-${name})
-    add_executable(${name})
-    add_executable(${PROJECT_NAME}::program::${base} ALIAS ${name})
-    target(SOURCES ${name} PRIVATE ${file})
-    target_link_libraries(${name}
+  file(GLOB entries CONFIGURE_DEPENDS "${PROJECT_SOURCE_DIR}/src/bin/*")
+  foreach (entry IN LISTS entries)
+    if (IS_DIRECTORY "${entry}")
+      get_filename_component(name "${entry}" NAME)
+      list(GLOB_RECURSE files CONFIGURE_DEPENDS "${entry}/*")
+    else()
+      get_filename_component(name "${entry}" NAME_WE)
+      set(files ${entry})
+    endif()
+    set(target ${PROJECT_NAME}-program-${name})
+    add_executable(${target})
+    add_executable(${PROJECT_NAME}::program::${name} ALIAS ${target})
+    target(SOURCES ${target} PRIVATE ${files})
+    target_link_libraries(${target}
       PRIVATE
         $<TARGET_NAME_IF_EXISTS:${PROJECT_NAME}::library>
         ${PROJECT_NAME}::private)
-    set_target_properties(${name}
+    set_target_properties(${target}
       PROPERTIES
-        OUTPUT_NAME ${base}
-        EXPORT_NAME program::${base})
-    set_property(GLOBAL APPEND PROPERTY coven::${PROJECT_NAME}::install ${name})
+        EXPORT_NAME program::${name}
+        OUTPUT_NAME ${name})
   endforeach()
 endfunction()
+
+function (coven_project_name)
+  set(primary ${PROJECT_NAME}-library)
+  if (NOT TARGET ${PROJECT_NAME}-library)
+    set(primary ${PROJECT_NAME})
+  endif()
+  add_library(${PROJECT_NAME}::${PROJECT_NAME} ALIAS ${primary})
+endfunction()
+
+#[[Generates a single object library (NOT a C++ module NOR a CMake Module]]
+function (coven_project_module directory)
+  file(GLOB sources LIST_DIRECTORIES OFF CONFIGURE_DEPENDS "${directory}/*")
+  # FIXME: Rust currently supports turning directories with a main file into a
+  # full executable. We should do this too before the blueprint is considered
+  # stable
+  list(FILTER sources EXCLUDE REGEX "^${directory}/main[.].*$")
+  if (NOT sources)
+    return()
+  endif()
+  get_filename_component(name "${directory}" NAME)
+  set(library ${PROJECT_NAME}-library-${name})
+  add_library(${library} OBJECT)
+  add_library(${PROJECT_NAME}::library::${name} ALIAS ${library})
+  set_property(TARGET ${library} PROPERTY EXCLUDE_FROM_ALL ON)
+  target(SOURCES ${library} PRIVATE ${sources})
+  target_include_directories(${library}
+    PUBLIC
+      $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
+    PRIVATE
+      $<BUILD_INTERFACE:${directory}>)
+  set(property $<TARGET_PROPERTY:${library},WHEN>)
+  string(CONCAT when $<IF:
+    $<BOOL:${property}>,
+    $<TARGET_GENEX_EVAL:${library},${property}>,
+    $<BOOL:ON>
+  >)
+  target_sources(${PROJECT_NAME}-library PRIVATE $<${when}:$<TARGET_OBJECTS:${library}>>)
+endfunction()
+
+
